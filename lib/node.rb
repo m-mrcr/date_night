4692161b4549e876a5e1db7443f98ac2f2a9c7f4
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-15 09:50:50 -0600

    initial commit

--- lib/node.rb
+++ lib/node.rb
@@ -1,11 +1,11 @@
 class Node
 
-  attr_reader :rating, :name
+  attr_reader :rating, :title
   attr_accessor :left, :right
 
-  def initialize(rate, name)
-    @rating = rating
-    @name = name
+  def initialize(attributes)
+    @rating = attributes[:rating]
+    @title = attributes[:title]
     left = nil
     right = nil
   end
--- test/node.rb
+++ test/node.rb
@@ -2,11 +2,17 @@ require './test/test_helper'
 
 class NodeTest < MiniTest::Test
   def setup
-    node = Node.new(91, "Cloud Atlas")
+    @node = Node.new(91, "Cloud Atlas")
   end
 
   def test_it_exists
-    assert_equal Node, node.class
+    assert_equal Node, @node.class
   end
 
+  def test_it_has_attributes
+    assert_equal 91, @node.rating
+    assert_equal "Cloud Atlas", @node.title
+    assert_nil @node.left
+    assert_nil @node.right
+  end
 end
