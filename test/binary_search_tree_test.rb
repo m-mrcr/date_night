a03ccb26cb1d77dad03eccda3a8579517d811b88
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-15 10:58:15 -0600

    adds tests through min method

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -14,4 +14,56 @@ class BinarySearchTreeTest < MiniTest::Test
     assert_equal 0, @tree.insert(61, "Bill & Ted's Excellent Adventure")
   end
 
+  def test_it_can_insert_multiple_nodes
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+
+    assert_equal 1, @tree.insert(16, "Johnny English")
+    assert_equal 1, @tree.insert(92, "Sharknado 3")
+    assert_equal 2, @tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
+
+  def test_it_can_verify_presence_of_rating_in_tree
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+
+    assert @tree.include?(61)
+  end
+
+  def test_it_can_reject_presence_of_rating_in_tree
+    refute @tree.include?(61)
+  end
+
+  def test_it_can_report_level_of_depth_where_rating_occurs_in_tree
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert 1, @tree.depth_of(92)
+    assert 2, @tree.depth_of(50)
+  end
+
+  def test_it_can_find_highest_rating
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+    @tree.insert(92, "Sharknado 3")
+
+    expected = {"Sharknado 3"=>92}
+
+    assert_equal expected, @tree.max
+  end
+
+  def test_it_can_find_lowest_rating
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(16, "Johnny English")
+    @tree.insert(61, "Bill & Ted's Excellent Adventure")
+    @tree.insert(50, "Hannibal Buress: Animal Furnace")
+    @tree.insert(92, "Sharknado 3")
+
+    expected = {"Johnny English"=>16}
+
+    assert_equal expected, @tree.min
+  end
+
 end
